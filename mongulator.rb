6e29ac79b2b3cee7fab5ec792bf98390481b2f0e
Elliot Crosby-McCullough and Makoto Inoue <bambinos+elliotcm+makoto@new-bamboo.co.uk>
2010-05-07 13:51:19 +0100

    Refactoring - Removed all comments.

--- mongulator.rb
+++ mongulator.rb
@@ -24,26 +24,13 @@ end
 
 # mput
 post '/insert' do
-  # db = scoped_database(params['name'])
-  # if coll.count < 200
-    json = JSON.parse(params['doc'])
-    new_json = {}
-    json.each{|key, value|
-      new_json[scoped_key(key)] = value
-    }
-    db.mput(new_json)
-  # end
+  json = JSON.parse(params['doc'])
+  new_json = {}
+  json.each{|key, value|
+    new_json[scoped_key(key)] = value
+  }
+  db.mput(new_json)
 end
-
-# post '/update' do
-#   coll   = scoped_collection(params['name'])
-#   query  = JSON.parse(params['query'])
-#   doc    = JSON.parse(params['doc'])
-#   upsert = (params['upsert'] == 'true')
-#   multi  = (params['multi'] == 'true')
-#   coll.update(query, doc, :upsert => upsert, :multi => multi)
-# end
-# 
 
 # out
 post '/remove' do
@@ -53,27 +40,16 @@ end
 
 # mget, get
 post '/find' do
-  # coll   = scoped_collection(params['name'])
-  # coll   = scoped_collection(params['name'])
   keys  = JSON.parse(params['query']).keys
-  # raise keys.inspect
-  
-  # fields = JSON.parse(params['fields'])
-  # fields = nil if fields == {}
-  # limit  = params['limit'].to_i
-  # skip   = params['skip'].to_i
-  # cursor = coll.find(query, :fields => fields, :limit => limit, :skip => skip)
   cursor = [] 
   unless keys.empty?
-    # raise keys.inspect
     cursor << db.mget(keys.map{|key| scoped_key(key)})
   else
     db.each do  |key, value|
-      if key.match(user_scope)
-        cursor << {key.sub(/^#{user_scope}\./, '') => value}
-      end
+      next unless key.match(user_scope)
+      cursor << {key.sub(/^#{user_scope}\./, '') => value}
     end
   end
-  # raise cursor.inspect
+
   return JSON.generate(cursor)
 end
